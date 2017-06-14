380fbbc69ce2e7dbc8f5e6ca50228d49fc77a09f
Benjamin Franzke <benjaminfranzke@gmail.com>
2017-06-14 11:53:12 +0200

    js: Add a es2015 compatible bundle
    Which includes as js modules and exports them.
    
    TODO: check whether this helps tools like rollup
    to selectively bundle modules when using a statement like:
    
    import { Carousel, Popover } from 'bootstrap';
    
    HEADS UP!: This breaks current browserify builds that use
    require('bootstrap') to load the umd-bundle.
    Users that want to keep the umd bundle need to define
    a "browser" statement in their package.json like:
    
    "browser": {
      "bootstrap": "./node_modules/bootstrap/dist/js/bootstrap.js"
    }
    
    (By using the node_module as relative path it's now treated as
    a local module by browserify so that it'll not use the
    browserify transform nor the "main" property we specify in our package.json)

---
+++ js/bootstrap.js
@@ -0,0 +1,23 @@
+import Alert from './src/alert'
+import Button from './src/button'
+import Carousel from './src/carousel'
+import Collapse from './src/collapse'
+import Dropdown from './src/dropdown'
+import Modal from './src/modal'
+import ScrollSpy from './src/scrollspy'
+import Tab from './src/tab'
+import Tooltip from './src/tooltip'
+import Popover from './src/popover'
+
+export {
+  Alert,
+  Button,
+  Carousel,
+  Collapse,
+  Dropdown,
+  Modal,
+  ScrollSpy,
+  Tab,
+  Tooltip,
+  Popover,
+}
