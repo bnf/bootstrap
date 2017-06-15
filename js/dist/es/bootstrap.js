ef62651d030414424d22c631319149ccbac8d3ba
Benjamin Franzke <benjaminfranzke@gmail.com>
2017-06-15 18:03:33 +0200

    Add es2015 modules which can be used by webpack and rollup

---
+++ js/dist/es/bootstrap.js
@@ -0,0 +1,11 @@
+export { default as Alert } from './alert';
+export { default as Button } from './button';
+export { default as Carousel } from './carousel';
+export { default as Collapse } from './collapse';
+export { default as Dropdown } from './dropdown';
+export { default as Modal } from './modal';
+export { default as ScrollSpy } from './scrollspy';
+export { default as Tab } from './tab';
+export { default as Tooltip } from './tooltip';
+export { default as Popover } from './popover';
+//# sourceMappingURL=bootstrap.js.map
